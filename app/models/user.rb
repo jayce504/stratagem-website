aac25783f0b4093440e29205e7844d7674c40e44
Scott Hudson <shudson5523@gmail.com>
2018-02-21 03:24:03 +0000

    Added secure passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
     before_save { self.email = email.downcase }
     
     has_many :clients
+    has_secure_password
 end 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221015549) do
+ActiveRecord::Schema.define(version: 20180221031520) do
 
   create_table "clients", force: :cascade do |t|
     t.string  "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20180221015549) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
