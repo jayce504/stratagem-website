1f7531aeaf75f032c630cd5251144984408bbc78
Scott Hudson <shudson5523@gmail.com>
2018-03-03 19:22:12 +0000

    Generated categories migration and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180303182920) do
+ActiveRecord::Schema.define(version: 20180303192057) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "clients", force: :cascade do |t|
     t.string  "name"
